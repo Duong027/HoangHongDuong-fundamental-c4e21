f8330b274ec2f80943f0afce928dd08fe766200f
duong027 <duong027@gmail.com>
2018-09-08 10:27:38 +0700

    initial commit

---
+++ session6_lab_2/song.py
@@ -0,0 +1,48 @@
+#Crawl list from iTunes
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from youtube_dl import YoutubeDL
+
+url = 'https://www.apple.com/itunes/charts/songs'
+conn = urlopen(url)
+
+raw_data = conn.read()
+html_page = raw_data.decode('utf-8')
+
+soup = BeautifulSoup(html_page, 'html.parser')
+div_list = soup.find_all('div', 'section-content')
+li_list = div_list[1].ul.find_all('li')
+
+song_list = []
+
+for li in li_list:
+    a = li.h3.a
+    title = li.h3.a.string
+    artist = li.h4.a.string
+    link = li.h3.a['href']
+    song = {
+        'Title': title,
+        'Artist': artist,
+        'Link': link,
+    }
+    song_list.append(song)
+# Export data ra excel
+import pyexcel
+pyexcel.save_as(records=song_list, dest_file_name='ITUNES.xlsx')
+
+#Download from Youtube
+
+dl = YoutubeDL()
+
+options = {
+    'default_search': 'ytsearch',
+    'max_download': 1,
+}
+
+dl = YoutubeDL(options)
+list_song = []
+for song in song_list:
+    song = song['Artist'] + ' ' + song['Title']
+    list_song.append(song)
+    print(song)
+dl.download(list_song)
